258c0d471b6b8bea2a01c65e92e0f472db46c2eb
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-04-27 12:03:17 +0100

    worksheet 2 (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use std::io; // import io module
+
 fn main() {
-    const KITTEN: &str = "🐱";
-    println!("look at this {}", KITTEN);
+  const KITTEN: &str = "🐱";
+  println!("Hi {}", KITTEN);
+
+  let number_of_files = 1;
+  println!("{} file(s) need to be opened.", number_of_files);
+
+  println!("How many files do you want to open?");
+
+  // code will not compile until you declare ‘input’ here
+  match io::stdin().read_line(&mut input) {
+    Ok(n) => {
+      println!("{} bytes read", n);
+      println!("Your input was: ({})", input); // note the parentheses
+    }
+    Err(error) => println!("Error while reading your input: {}", error),
+  }
+
+  println!("Bye {}", KITTEN);
 }
