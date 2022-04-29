63bca1b7e977c181d34e4c544bea1cf423484fd1
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-04-29 15:10:52 +0100

    worksheet2 (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
-use std::io; // import io module
+use std::io; 
 
 fn main() {
   const KITTEN: &str = "🐱";
   println!("Hi {}", KITTEN);
 
-  let number_of_files = 1;
-  println!("{} file(s) need to be opened.", number_of_files);
+  let mut number_of_files: u32 = 1;
+  println!("default number_of_files={}", number_of_files);
 
   println!("How many files do you want to open?");
 
+  let mut input: String = String::new();
   // code will not compile until you declare ‘input’ here
   match io::stdin().read_line(&mut input) {
-    Ok(n) => {
-      println!("{} bytes read", n);
-      println!("Your input was: ({})", input); // note the parentheses
-    }
+    Ok(_n) => {}
     Err(error) => println!("Error while reading your input: {}", error),
+  }
+
+  input.pop();  
+
+  number_of_files = input.parse().unwrap_or(1);
+  let n = number_of_files;
+  println!("updated number_of_files={}", number_of_files);
+
+  while number_of_files != 0 {
+    println!("Please enter the name/path to a file: ");
+    // need to ask for input here
+ 
+    number_of_files -= 1; // or: number_of_files = number_of_files - 1;
   }
 
   println!("Bye {}", KITTEN);
 }
+
