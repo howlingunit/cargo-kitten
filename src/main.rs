a3d5d0a6aeb1556b8108bb893e3e3d52d156d0eb
howlingunit <93931085+howlingunit@users.noreply.github.com>
2022-05-13 14:11:53 +0100

    cli updates

--- src/main.rs
+++ src/main.rs
@@ -6,50 +6,61 @@ fn main() {
   const KITTEN: &str = "🐱";
   println!("Hi {}", KITTEN);
 
-  let mut filenames: Vec<String>;
+  let mut filenames: Vec<String> = vec![].to_vec();
 
   for i in env::args(){
     filenames.push(i);
   }
+  
+  filenames.remove(0); // remove path to file
 
-  let mut number_of_files: u32 = 1;
-  println!("default number_of_files={}", number_of_files);
-
-  println!("How many files do you want to open?");
-
-  let mut input: String = String::new();
-  match io::stdin().read_line(&mut input) {
-    Ok(_n) => {
-      input = first_word(input);
-      number_of_files = input.parse().unwrap_or(1);
-    }
-    Err(error) => println!("Error while reading your input: {}", error),
-  }
-
-  println!("number_of_files={}", number_of_files);
-
-  let mut output = String::new();
-
-  for i in 1..(number_of_files + 1) {
-    let ordinal = to_ordinal(i);
-    input = String::from("");
-
-    println!(
-      "[{}/{}]Please enter the name/path to the {} file: ",
-      i, number_of_files, ordinal
-    );
-    match io::stdin().read_line(&mut input) {
-      Ok(_) => {
-        input = first_word(input);
-        println!("Opening file: {}", input); // for debugging
-      }
-      Err(error) => {
-        panic!("Error while reading your input: {}", error);
-      }
-    }
-    output = output + &file_content(&input).unwrap();
+  let mut output: String = String::new();
+  
+  for i in filenames {
+    output = output + &file_content(&i).unwrap();
+    output = output + "\n";
   }
   println!("{}", output);
 
   println!("Bye {}", KITTEN);
 }
+
+  // old stuff
+
+  // let mut number_of_files = filenames.len();
+  // println!("default number_of_files={}", number_of_files);
+
+  // println!("How many files do you want to open?");
+
+  // let mut input: String = String::new();
+  // match io::stdin().read_line(&mut input) {
+  //   Ok(_n) => {
+  //     input = first_word(input);
+  //     number_of_files = input.parse().unwrap_or(1);
+  //   }
+  //   Err(error) => println!("Error while reading your input: {}", error),
+  // }
+
+  // println!("number_of_files={}", number_of_files);
+
+  // let mut output = String::new();
+
+  // for i in 1..(number_of_files + 1) {
+  //   let ordinal = to_ordinal(i);
+  //   input = String::from("");
+
+  //   println!(
+  //     "[{}/{}]Please enter the name/path to the {} file: ",
+  //     i, number_of_files, ordinal
+  //   );
+  //   match io::stdin().read_line(&mut input) {
+  //     Ok(_) => {
+  //       input = first_word(input);
+  //       println!("Opening file: {}", input); // for debugging
+  //     }
+  //     Err(error) => {
+  //       panic!("Error while reading your input: {}", error);
+  //     }
+  //   }
+  //   output = output + &file_content(&input).unwrap();
+  // }
